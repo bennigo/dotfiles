cfd162fe42d041e638c34ed5be82150d26d4551b
Benedikt Gunnar Ófeigsson <bgo@vedur.is>
2024-02-13 16:04:12 +0000

    adding neovim, autebrowser zsh tmux to dotfile repo

---
+++ neovim/.config/nvim/lua/default/lazy.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+--  You can configure plugins using the `config` key.
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup({
+  { import = 'default.plugins' },
+  { import = 'default.plugins.lsp' },
+  }, {
+    install = { 
+     colorscheme = { "habamax" } 
+   },
+   checker = { enabled = true, notify = false },
+   change_detection = { notify = false },
+})
+-- 
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+
+-- vim.cmd.colorscheme("kanagawa-wave")
---
+++ neovim/.config/nvim/lua/default/plugins/init.lua
@@ -0,0 +1,34 @@
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth',
+  -- "tpope/vim-surround",
+
+--  {
+--    -- Add indentation guides even on blank lines
+--    'lukas-reineke/indent-blankline.nvim',
+--    -- Enable `lukas-reineke/indent-blankline.nvim`
+--    -- See `:help ibl`
+--    main = 'ibl',
+--    opts = {},
+--  },
+  -- "gc" to comment visual regions/lines
+--   {
+--     'numToStr/Comment.nvim',
+--     event = { 'BufNewFile', 'BufReadPre' },
+--     config = true,
+--   },
+--   {
+--     'ibhagwan/fzf-lua',
+--     -- optional for icon support
+--     dependencies = { 'nvim-tree/nvim-web-devicons' },
+--     config = function()
+--       -- calling `setup` is optional for customization
+--       require('fzf-lua').setup {}
+--     end,
+--   },
+
+    -- tests
+    
+}
