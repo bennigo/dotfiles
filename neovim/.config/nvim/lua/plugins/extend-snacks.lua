8e1ba594f05d8e309e27acaa7e42b02671b07764
Benedikt Gunnar Ófeigsson <bgo@vedur.is>
2025-07-13 23:59:01 +0000

    changing some nvim plugins

--- neovim/.config/nvim/lua/plugins/extend-snacks.lua
+++ neovim/.config/nvim/lua/plugins/extend-snacks.lua
@@ -1,12 +1,21 @@
 return {
   "folke/snacks.nvim",
-  enable = true,
+  enabled = true,
   opts = function(_, opts)
-    table.insert(
-      opts.dashboard.preset.keys,
-      7,
-      { icon = "S", key = "S", desc = "Select Session", action = require("persistence").select }
-    )
+    local final_opts = vim.tbl_deep_extend("force", {
+      image = {
+        enabled = true,
+      },
+    }, opts)
+
+    table.insert(final_opts.dashboard.preset.keys, 7, {
+      icon = "S",
+      key = "S",
+      desc = "Select Session",
+      action = require("persistence").select,
+    })
+
+    return final_opts
   end,
   keys = {
     { "<leader>e", false },
